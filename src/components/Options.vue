cc0bef3591e1a9198347fd2232f846b67b6d2183
Stas Stepaniuk <ssttaass0415@gmail.com>
2024-02-13 14:19:43 +0200

    Added section for toggling options

---
+++ src/components/Options.vue
@@ -0,0 +1,45 @@
+<script>
+import OptionsCheckbox from "@/components/OptionsCheckbox.vue";
+
+export default {
+  name: "Options",
+  components: {OptionsCheckbox},
+  data() {
+    return {
+      options: [
+        {
+          id: 1,
+          value: "Use capital letters"
+        },
+        {
+          id: 2,
+          value: "Use numbers"
+        },
+        {
+          id: 3,
+          value: "Use special symbols"
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="options">
+    <span class="options-title">Options:</span>
+    <transition-group name="options">
+      <options-checkbox v-for="option in this.options" :option="option.value" :key="option.id"/>
+    </transition-group>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.options {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: .6em;
+}
+</style>
---
+++ src/components/OptionsCheckbox.vue
@@ -0,0 +1,75 @@
+<script>
+export default {
+  name: "OptionsCheckbox",
+  props: {
+    option: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+  <label class="checkbox">
+    {{ this.option }}
+    <input type="checkbox">
+    <span class="checkbox-checkmark"></span>
+  </label>
+</template>
+
+<style scoped lang="scss">
+@import "@/assets/scss/variables";
+
+.checkbox {
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  cursor: pointer;
+  user-select: none;
+  padding-left: 2.1em;
+  padding-top: .3em;
+
+  & input {
+    position: absolute;
+    height: 0;
+    width: 0;
+    opacity: 0;
+    cursor: pointer;
+  }
+
+  & input:checked ~ &-checkmark {
+    background: $highlight-color;
+  }
+
+  & &-checkmark:after {
+    left: .6em;
+    top: .3em;
+    width: .3em;
+    height: .6em;
+    border: solid #ffffff;
+    border-width: 0 .2em .2em 0;
+    transform: rotate(45deg);
+  }
+
+  & input:checked ~ &-checkmark:after {
+    display: block;
+  }
+
+  &-checkmark {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 1.6em;
+    width: 1.6em;
+    border-radius: .3em;
+    background-color: #ffffff;
+
+    &:after {
+      content: "";
+      position: absolute;
+      display: none;
+    }
+  }
+}
+</style>
