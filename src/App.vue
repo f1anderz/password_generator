1ed3e636cb239466a4659e7f2044678165f5b15d
Stas Stepaniuk <ssttaass0415@gmail.com>
2024-02-13 19:30:58 +0200

    Placed all components correctly in the container

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,29 @@ import PasswordLength from "@/components/PasswordLength.vue";
 import Options from "@/components/Options.vue";
 import Algorithm from "@/components/Algorithm.vue";
 import AlertWindow from "@/components/AlertWindow.vue";
+import GenerateButton from "@/components/GenerateButton.vue";
 
 export default {
   name: "App",
-  components: {AlertWindow, Algorithm, Options, PasswordLength, PasswordInput},
+  components: {GenerateButton, AlertWindow, Algorithm, Options, PasswordLength, PasswordInput},
 }
 </script>
 
 <template>
   <div class="wrapper">
     <div class="wrapper-container">
-      <password-input @passCopy=""/>
-      <password-length/>
-      <options/>
-      <algorithm/>
+      <div class="wrapper-container-title">
+        Password<span class="wrapper-container-title-highlighted">Generator</span>
+      </div>
+      <div class="wrapper-container-row">
+        <password-input @passCopy=""/>
+        <password-length/>
+      </div>
+      <div class="wrapper-container-row">
+        <options/>
+        <algorithm/>
+      </div>
+      <generate-button/>
     </div>
     <alert-window class="wrapper-message"/>
   </div>
@@ -32,22 +41,43 @@ export default {
   height: 100vh;
   position: relative;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+  gap: 1rem;
 
   &-container {
     width: 40%;
     margin: 0 auto;
+    padding: 2vh 0;
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
     flex-wrap: wrap;
     gap: 1em;
     background: $background-color;
     border-radius: 1em;
-    padding: 5vh 0;
+
+    &-title {
+      display: flex;
+      font-size: 2rem;
+      gap: .8rem;
+      font-weight: bold;
+
+      &-highlighted {
+        color: $highlight-color;
+      }
+    }
+
+    &-row {
+      width: 80%;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+      flex-wrap: wrap;
+    }
   }
 
   &-message {
