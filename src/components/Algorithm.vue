0b200c1baab3cf71349717663deb10ef1cbe45f3
Stas Stepaniuk <ssttaass0415@gmail.com>
2024-02-13 14:19:24 +0200

    Added section to select algorithm

---
+++ src/components/Algorithm.vue
@@ -0,0 +1,45 @@
+<script>
+import AlgorithmRadio from "@/components/AlgorithmRadio.vue";
+
+export default {
+  name: "Algorithm",
+  components: {AlgorithmRadio},
+  data() {
+    return {
+      radioGroup: "algorithm",
+      algorithms: [
+        {
+          id: 1,
+          value: "Normal",
+          checked: true
+        },
+        {
+          id: 2,
+          value: "Enhanced",
+          checked: false
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="algorithm">
+    <span class="algorithm-title">Algorithm:</span>
+    <transition-group name="algorithm">
+      <algorithm-radio v-for="algorithm in this.algorithms" :key="algorithm.id" :option="algorithm.value"
+                       :checked="algorithm.checked" :group="this.radioGroup"/>
+    </transition-group>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.algorithm {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: .4em;
+}
+</style>
---
+++ src/components/AlgorithmRadio.vue
@@ -0,0 +1,83 @@
+<script>
+export default {
+  name: "AlgorithmRadio",
+  props: {
+    option: {
+      type: String,
+      required: true
+    },
+    checked: {
+      type: Boolean,
+      required: true
+    },
+    group: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+  <label class="radio">
+    {{ this.option }}
+    <input type="radio" :name="this.group" :checked="this.checked">
+    <span class="radio-checkmark"></span>
+  </label>
+</template>
+
+<style scoped lang="scss">
+@import "@/assets/scss/variables";
+
+.radio {
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  cursor: pointer;
+  user-select: none;
+  padding-left: 2.1em;
+  padding-top: .3em;
+
+  & input {
+    position: absolute;
+    height: 0;
+    width: 0;
+    opacity: 0;
+    cursor: pointer;
+  }
+
+  & input:checked ~ &-checkmark {
+    background: $highlight-color;
+  }
+
+  & &-checkmark:after {
+    left: .58em;
+    top: .3em;
+    width: .3em;
+    height: .6em;
+    border: solid #ffffff;
+    border-width: 0 .2em .2em 0;
+    transform: rotate(45deg);
+  }
+
+  & input:checked ~ &-checkmark:after {
+    display: block;
+  }
+
+  &-checkmark {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 1.6em;
+    width: 1.6em;
+    border-radius: 50%;
+    background-color: #ffffff;
+
+    &:after {
+      content: "";
+      position: absolute;
+      display: none;
+    }
+  }
+}
+</style>
