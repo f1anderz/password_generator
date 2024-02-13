2b2ae00fdc0b41f56bcaea09b7a43e8e9a6b7be8
Stas Stepaniuk <ssttaass0415@gmail.com>
2024-02-13 14:20:17 +0200

    Added password input for user copy and Length input

---
+++ src/components/PasswordInput.vue
@@ -0,0 +1,59 @@
+<script>
+export default {
+  name: "PasswordInput",
+  data() {
+    return {
+      password: "",
+    }
+  }, methods: {
+    async copyPassword() {
+      await navigator.clipboard.writeText(this.password);
+      this.$emit('passCopy');
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="password-container">
+    <input class="password-container-input" v-model="this.password" type="text" placeholder="Your Password...">
+    <img class="password-container-copy" src="@/assets/img/copy.svg" alt="Copy"
+         @click="this.copyPassword">
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import "@/assets/scss/variables";
+
+.password-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 50%;
+  background: #ffffff;
+  border: 2px solid $highlight-color;
+  border-radius: .5em;
+  padding: .2em;
+
+  &-input {
+    display: block;
+    width: 95%;
+    padding: .1em .3em;
+    font-size: 110%;
+    border: none;
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+  &-copy {
+    width: 1.6em;
+    height: 1.6em;
+
+    &:hover {
+      cursor: pointer;
+    }
+  }
+}
+</style>
---
+++ src/components/PasswordLength.vue
@@ -0,0 +1,86 @@
+<script>
+export default {
+  name: "PasswordLength",
+  data() {
+    return {
+      lengthValue: 12
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="password-length">
+    <div class="password-length-label">Length:</div>
+    <div class="password-length-container">
+      <input class="password-length-container-input" type="number" v-model="this.lengthValue">
+      <div class="password-length-container-arrows">
+        <img class="password-length-container-arrows-up" src="../assets/img/arrow.svg" alt="Up"
+             @click="this.lengthValue += 1">
+        <img class="password-length-container-arrows-down" src="../assets/img/arrow.svg" alt="Down"
+             @click="this.lengthValue -= 1">
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import "@/assets/scss/variables";
+
+.password-length {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: .4em;
+  width: 16%;
+
+  &-container {
+    display: flex;
+    flex-direction: row;
+    gap: .3em;
+    padding: 0 .3em;
+    background: #ffffff;
+    border: 2px solid $highlight-color;
+    border-radius: .3em;
+
+    &-input {
+      display: block;
+      text-align: center;
+      width: 100%;
+      border: none;
+      -moz-appearance: textfield;
+      background: #ffffff;
+
+      &:focus {
+        outline: none;
+      }
+
+      &::-webkit-inner-spin-button, &::-webkit-outer-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+      }
+    }
+
+    &-arrows {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+
+      &-up {
+        transform: rotate(180deg);
+      }
+
+      &-up, &-down {
+        display: block;
+        width: .9em;
+        height: .9em;
+
+        &:hover {
+          cursor: pointer;
+        }
+      }
+    }
+  }
+}
+</style>
